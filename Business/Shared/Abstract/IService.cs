831219e330d708edbe55658efa47552d0b3f2932
emreguleer <guler.emre.99@gmail.com>
2024-12-30 18:04:58 +0300

    Generic Service eklendi.

---
+++ Business/Shared/Abstract/IService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Shared.Abstract
+{
+    public interface IService<T> where T : class
+    {
+        T Add(T entity);
+        T Update(T entity);
+        IQueryable<T> GetAll();
+        IQueryable<T> GetAll(Expression<Func<T, bool>> predicate);
+        T GetById(int id);
+        bool HardDelete(int id);
+    }
+}
---
+++ Business/Shared/Concrete/Service.cs
@@ -0,0 +1,53 @@
+﻿using Business.Shared.Abstract;
+using Repository.Shared.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Shared.Concrete
+{
+    public class Service<T> : IService<T> where T : class
+    {
+        private readonly IRepository<T> _repo;
+
+        public Service(IRepository<T> repo)
+        {
+            _repo = repo;
+        }
+
+        public T Add(T entity)
+        {
+            return _repo.Add(entity);
+        }
+
+        public IQueryable<T> GetAll()
+        {
+            return _repo.GetAll();
+        }
+
+        public IQueryable<T> GetAll(Expression<Func<T, bool>> predicate)
+        {
+            return _repo.GetAll(predicate);
+        }
+
+
+        public T GetById(int id)
+        {
+            return _repo.GetById(id);
+        }
+
+
+        public bool HardDelete(int id)
+        {
+            return _repo.HardDelete(id);
+        }
+
+        public T Update(T entity)
+        {
+            return _repo.Update(entity);
+        }
+    }
+}
